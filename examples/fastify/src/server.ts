75d658bd49dd10aa7b052f1a31922055b42761a3
ridafkih <hello@rida.dev>
2026-02-17 20:27:42 -0800

    fix: add example.log to examples and ensure examples are configured correctly

--- examples/fastify/src/server.ts
+++ examples/fastify/src/server.ts
@@ -2,10 +2,12 @@ import fastify from "fastify";
 import { destroy } from "./logger";
 import { logging } from "./plugins/logging";
 import { checkoutRoutes } from "./routes/checkout";
+import { healthRoutes } from "./routes/health";
 
 const app = fastify();
 
 app.register(logging);
+app.register(healthRoutes);
 app.register(checkoutRoutes);
 
 app.listen({ port: 3000 }, () => {
